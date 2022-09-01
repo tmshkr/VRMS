6555f01b2c83aff68dfd75c0645303f9bdebbe62
Tim Shaker <tim@tmshkr.com>
2022-08-31 19:50:22 -0700

    connect slack

--- apps/web/pages/api/auth/[...nextauth].js
+++ apps/web/pages/api/auth/[...nextauth].js
@@ -1,13 +1,165 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GitHubProvider from "next-auth/providers/github";
+import prisma from "lib/prisma";
+import { getMongoClient } from "lib/mongo";
+const Cookies = require("cookies");
 
-export default NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-    // ...add more providers here
-  ],
-});
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+
+const nextAuthOptions = (req, res) => {
+  const cookies = new Cookies(req, res, {
+    keys: process.env.COOKIE_KEYS.split(" "),
+  });
+
+  return {
+    providers: [
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+    ],
+    theme: {
+      colorScheme: "light",
+    },
+    callbacks: {
+      async signIn(args) {
+        return true;
+      },
+      async redirect({ url, baseUrl }) {
+        return baseUrl;
+      },
+      async session(args) {
+        const { session, token } = args;
+        const {
+          gh_username,
+          provider,
+          provider_account_id,
+          two_factor_authentication,
+        } = token;
+
+        session.user.gh_username = gh_username;
+        session.user.two_factor_authentication = two_factor_authentication;
+
+        // cache vrms_user in a cookie to prevent excessive db queries
+        try {
+          var vrms_user = JSON.parse(
+            cookies.get("vrms_user", { signed: true }) || null
+          );
+        } catch (err) {
+          console.error("Error getting vrms_user cookie: ", err);
+        }
+
+        if (!vrms_user) {
+          vrms_user = await prisma.account
+            .findUnique({
+              where: {
+                provider_provider_account_id: {
+                  provider,
+                  provider_account_id,
+                },
+              },
+              select: {
+                user: {
+                  select: { id: true, slack_id: true, app_roles: true },
+                },
+              },
+            })
+            .then((data) => {
+              if (!data) return null;
+              const { user } = data;
+              return {
+                ...user,
+                app_roles: user.app_roles.map(({ role }) => role),
+              };
+            });
+
+          cookies.set("vrms_user", JSON.stringify(vrms_user), {
+            signed: true,
+            maxAge: 1000 * 10,
+          });
+        }
+
+        session.user.vrms_user = vrms_user;
+        return session;
+      },
+      async jwt(args) {
+        const { token, user, account, profile } = args;
+
+        // account, profile, and user are available during sign in
+        if (account && profile && user) {
+          const { name, email } = user;
+          const {
+            provider,
+            type,
+            providerAccountId: provider_account_id,
+            access_token,
+            token_type,
+            scope,
+          } = account;
+          const { login: gh_username, two_factor_authentication } = profile;
+
+          token.provider = provider;
+          token.provider_account_id = provider_account_id;
+          token.gh_username = gh_username;
+          token.scope = scope;
+          token.type = type;
+          token.access_token = access_token;
+          token.token_type = token_type;
+          token.two_factor_authentication = two_factor_authentication;
+
+          await prisma.account
+            .update({
+              where: {
+                provider_provider_account_id: { provider, provider_account_id },
+              },
+              data: {
+                access_token,
+                email,
+                gh_username,
+                name,
+                scope,
+                token_type,
+                type,
+                two_factor_authentication,
+              },
+            })
+            .catch(async (err) => {
+              const { meta } = err;
+              if (meta.cause === "Record to update not found.") {
+                console.log("User logged in without an existing VRMS account");
+                // save unconnected account to mongo
+                const mongoClient = await getMongoClient();
+                mongoClient.db().collection("unconnectedAccounts").updateOne(
+                  { provider, provider_account_id },
+                  {
+                    $set: {
+                      provider,
+                      provider_account_id,
+                      access_token,
+                      email,
+                      gh_username,
+                      name,
+                      scope,
+                      token_type,
+                      type,
+                      two_factor_authentication,
+                    },
+                  },
+                  { upsert: true }
+                );
+              } else {
+                console.error(err);
+              }
+            });
+        }
+
+        return token;
+      },
+    },
+  };
+};
+
+export default (req, res) => {
+  return NextAuth(req, res, nextAuthOptions(req, res));
+};
---
+++ apps/web/pages/api/connect/slack.js
@@ -0,0 +1,93 @@
+import jwt from "jsonwebtoken";
+import Cookies from "cookies";
+import { getToken } from "next-auth/jwt";
+import { getMongoClient } from "lib/mongo";
+import prisma from "lib/prisma";
+import { notifyAccountConnected } from "lib/slack";
+
+export default async function handler(req, res) {
+  const nextToken = await getToken({ req });
+  const cookies = new Cookies(req, res);
+
+  if (!nextToken) {
+    cookies.set("redirect_to", req.url, {
+      httpOnly: false,
+      overwrite: true,
+      sameSite: "strict",
+    });
+    res.redirect("/api/auth/signin");
+    return;
+  }
+
+  const {
+    access_token,
+    email,
+    name,
+    gh_username,
+    provider,
+    provider_account_id,
+    scope,
+    token_type,
+    type,
+    two_factor_authentication,
+  } = nextToken;
+
+  if (provider !== "github") {
+    res.status(501).send("Provider not implemented");
+    return;
+  }
+
+  try {
+    var { vrms_user_id, slack_id } = jwt.verify(
+      req.query.token,
+      process.env.NEXTAUTH_SECRET
+    );
+  } catch (err) {
+    console.error(err);
+    res.redirect("/api/auth/signin");
+    return;
+  }
+
+  try {
+    await prisma.account.create({
+      data: {
+        provider,
+        provider_account_id,
+        access_token,
+        email,
+        gh_username,
+        name,
+        scope,
+        token_type,
+        type,
+        two_factor_authentication,
+        vrms_user_id,
+      },
+    });
+
+    console.log("Account connected", {
+      vrms_user_id,
+      slack_id,
+      provider,
+      provider_account_id,
+      gh_username,
+    });
+
+    res.redirect("/");
+    notifyAccountConnected(slack_id, gh_username);
+
+    // delete any unconnectedAccount for this user
+    const mongoClient = await getMongoClient();
+    mongoClient
+      .db()
+      .collection("unconnectedAccounts")
+      .deleteOne({ provider, provider_account_id });
+  } catch (err) {
+    if (err.code === "P2002") {
+      res.send("It looks like this account is already connected");
+    } else {
+      console.error(err);
+      res.status(500).send("There was a problem connecting your account");
+    }
+  }
+}
