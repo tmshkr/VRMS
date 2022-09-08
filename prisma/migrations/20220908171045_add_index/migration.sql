8964562295b83fc121c13b1673d7fe7a5b46acdb
Tim Shaker <tim@tmshkr.com>
2022-09-08 10:34:58 -0700

    add index

---
+++ prisma/migrations/20220908171045_add_index/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "MeetingParticipant_user_id_idx" ON "MeetingParticipant"("user_id");
+
+-- CreateIndex
+CREATE INDEX "TeamMember_user_id_idx" ON "TeamMember"("user_id");
