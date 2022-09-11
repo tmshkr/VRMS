1807c75921aba6e164e80c5253a4ed5b5b75f895
Tim Shaker <tim@tmshkr.com>
2022-09-10 18:08:28 -0700

    auth middleware

--- apps/studio/src/index.ts
+++ apps/studio/src/index.ts
@@ -1,11 +1,47 @@
 import express from "express";
+import { jwtDecrypt } from "jose";
+import hkdf from "@panva/hkdf";
+
+import { PrismaClient } from "@prisma/client";
+const prisma: PrismaClient = new PrismaClient();
+
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const Cookies = require("cookies");
+
 const app = express();
 const port = 5000;
 
 // check for role on / and /api routes
-app.use((req, res) => {
-  res.status(501).send("Auth middleware not yet implemented");
+app.use([/^\/$/, "/api"], async (req, res, next) => {
+  const cookies = new Cookies(req, res);
+  const cookieName = process.env.NEXTAUTH_URL?.startsWith("https://")
+    ? "__Secure-next-auth.session-token"
+    : "next-auth.session-token";
+  const token = cookies.get(cookieName);
+  const payload: any = await decode({
+    token,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  const { provider, provider_account_id } = payload;
+  const app_roles: any = await prisma.account
+    .findUnique({
+      where: {
+        provider_provider_account_id: {
+          provider,
+          provider_account_id,
+        },
+      },
+      select: { user: { select: { app_roles: true } } },
+    })
+    .then((result) => {
+      if (!result) return [];
+      return result.user.app_roles.map(({ role }) => role);
+    });
+
+  if (app_roles.includes("ADMIN")) {
+    next();
+  } else res.status(401).send("Unauthorized");
 });
 
 app.use(
@@ -18,3 +54,23 @@ app.use(
 app.listen(port, () => {
   console.log(`Prisma Studio auth middleware proxy listening on port ${port}`);
 });
+
+async function decode(params) {
+  const { token, secret } = params;
+  if (!token) return null;
+  const encryptionSecret = await getDerivedEncryptionKey(secret);
+  const { payload } = await jwtDecrypt(token, encryptionSecret, {
+    clockTolerance: 15,
+  });
+  return payload;
+}
+
+async function getDerivedEncryptionKey(secret: string | Buffer) {
+  return await hkdf(
+    "sha256",
+    secret,
+    "",
+    "NextAuth.js Generated Encryption Key",
+    32
+  );
+}
