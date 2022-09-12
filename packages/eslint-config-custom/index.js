1c724866522cc49935d5aeacbd1777a8bda8f924
Tim Shaker <tim@tmshkr.com>
2022-09-11 23:23:47 -0700

    update eslint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,5 +4,7 @@ module.exports = {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
     "turbo/no-undeclared-env-vars": "off",
+    "@next/next/no-img-element": "off",
+    "react-hooks/exhaustive-deps": "off",
   },
 };
