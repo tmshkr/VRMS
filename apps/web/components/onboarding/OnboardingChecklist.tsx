e7170e7b38213598e817e296aa0be4f5661ce9b3
Tim Shaker <tim@tmshkr.com>
2022-09-14 12:10:58 -0700

    add user data to onboarding checklist

--- apps/web/components/onboarding/OnboardingChecklist.tsx
+++ apps/web/components/onboarding/OnboardingChecklist.tsx
@@ -1,37 +1,72 @@
+import { ChangeEventHandler } from "react";
+import { useAppSelector } from "src/store";
+import { selectUser } from "src/store/user";
+
 export const OnboardingChecklist = () => {
+  const user = useAppSelector(selectUser);
+  if (!user) return null;
+  const { two_factor_authentication, slack_id } = user;
+
   return (
     <fieldset className="space-y-5 w-fit m-auto">
       <ChecklistItem
-        checked={true}
+        checked={!!slack_id}
         name="connect-slack"
         label="Connect your account to Slack in the Home tab"
         description={
-          true
+          !!slack_id
             ? "Your Slack account is connected"
             : "Watch this video to see how to do it"
         }
       />
       <ChecklistItem
-        checked={true}
+        checked={two_factor_authentication}
         name="enable-2fa"
         label="Enable 2FA on your GitHub account"
         description={
-          true
+          two_factor_authentication
             ? "2FA is enabled on your GitHub account"
             : "Enable 2FA on your GitHub account for better security"
         }
       />
       <ChecklistItem
         checked={false}
+        readOnly={false}
+        onChange={(e) => console.log(e)}
         name="code-of-conduct"
-        label="Read and agree to the Code of Conduct"
-        description={`Look for the "key" in the Code of Conduct`}
+        label={
+          <>
+            Read and agree to the{" "}
+            <a
+              href="https://github.com/hackforla/codeofconduct"
+              target="_blank"
+              rel="noopener noreferer"
+            >
+              Code of Conduct
+            </a>
+          </>
+        }
+        description="Look for the magic word in the Code of Conduct"
       />
     </fieldset>
   );
 };
 
-const ChecklistItem = ({ checked, name, label, description }) => {
+const ChecklistItem = ({
+  checked,
+  name,
+  label,
+  description,
+  readOnly = true,
+  onChange,
+}: {
+  checked: boolean;
+  name: string;
+  label: any;
+  description: string;
+  readOnly?: boolean;
+  onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
+}) => {
   return (
     <div key={name} className="relative flex items-start">
       <div className="flex h-5 items-center">
@@ -40,6 +75,8 @@ const ChecklistItem = ({ checked, name, label, description }) => {
           aria-describedby={`${name}-description`}
           name={name}
           checked={checked}
+          readOnly={readOnly}
+          onChange={onChange}
           type="checkbox"
           className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
         />
