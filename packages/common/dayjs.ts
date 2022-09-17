cae986d9d302139b63377842abb33937f4b57d8a
Tim Shaker <tim@tmshkr.com>
2022-09-17 14:34:19 -0700

    dayjs extensions

--- packages/common/dayjs.ts
+++ packages/common/dayjs.ts
@@ -3,12 +3,16 @@ import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import weekday from "dayjs/plugin/weekday";
 import isoWeek from "dayjs/plugin/isoWeek";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault("America/Los_Angeles");
 dayjs.extend(weekday);
 dayjs.extend(isoWeek);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
 
 export function findOccurrenceOfWeekday(date: dayjs.Dayjs) {
   let n = 0;
