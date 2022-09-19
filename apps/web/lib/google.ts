7d06c133c99740dc6b2dffb68d7897e91809d814
Tim Shaker <tim@tmshkr.com>
2022-09-19 15:49:15 -0700

    delete lib/google

--- apps/web/lib/google.ts
+++
@@ -1,8 +0,0 @@
-import dayjs from "common/dayjs";
-export function generateBrowserEventInstanceId(gcal_event_id, nextMeeting) {
-  return btoa(
-    `${gcal_event_id}_${dayjs(nextMeeting)
-      .utc()
-      .format("YYYYMMDDTHHmmss[Z]")} ${process.env.GOOGLE_CALENDAR_ID}`
-  ).replace(/=/g, "");
-}
