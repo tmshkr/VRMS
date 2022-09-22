ff03fd411e861443f32c1e856a4311f4b688703a
Tim Shaker <tim@tmshkr.com>
2022-09-21 17:57:25 -0700

    create meetingException

--- packages/common/google/sync.ts
+++ packages/common/google/sync.ts
@@ -7,6 +7,11 @@ import { getEvents, getAuth } from "./index";
 import dayjs from "common/dayjs";
 const { google } = require("googleapis");
 
+enum EventType {
+  meeting = "meeting",
+  meetingException = "meetingException",
+}
+
 export async function syncMeetings() {
   const mongoClient = await getMongoClient();
   const doc = await mongoClient
@@ -21,6 +26,21 @@ export async function syncMeetings() {
 
   console.log(items);
 
+  const events = {};
+  const exceptions = {};
+
+  for (const item of items) {
+    if (/_/.test(item.id)) {
+      exceptions[item.id] = item;
+    } else {
+      events[item.id] = item;
+    }
+  }
+  console.log({ events, exceptions });
+
+  handleUpdate(EventType.meeting, events);
+  handleUpdate(EventType.meetingException, exceptions);
+
   await mongoClient
     .db()
     .collection("config")
@@ -32,60 +52,99 @@ export async function syncMeetings() {
       },
       { upsert: true }
     );
+}
 
-  const events = items.reduce((acc, cur) => {
-    acc[cur.id] = cur;
-    return acc;
-  }, {});
+async function handleUpdate(eventType: EventType, events) {
+  const eventIds = Object.keys(events);
+  let records;
+  if (eventType === EventType.meeting) {
+    records = await prisma.meeting.findMany({
+      where: { gcal_event_id: { in: eventIds } },
+    });
+  } else if (eventType === EventType.meetingException) {
+    records = await prisma.meetingException.findMany({
+      where: { gcal_event_id: { in: eventIds } },
+    });
+  }
 
-  const meetings = await prisma.meeting.findMany({
-    where: { gcal_event_id: { in: Object.keys(events) } },
+  // find the events that weren't found in the database,
+  // so that we can create them, as long as they have a vrms_meeting_id
+  const eventsToCreate = new Set(eventIds);
+  for (const meeting of records) {
+    if (eventsToCreate.has(meeting.gcal_event_id)) {
+      eventsToCreate.delete(meeting.gcal_event_id);
+    }
+  }
+
+  eventsToCreate.forEach(async (eventId) => {
+    const event = events[eventId];
+    const meeting_id = Number(event.extendedProperties.private.vrms_meeting_id);
+    if (meeting_id) {
+      if (eventType === EventType.meeting) {
+        // await prisma.meeting.create({
+        //   data: {å
+        //     gcal_event_id: String(event.id),
+        //   },
+        // });
+      } else if (eventType === EventType.meetingException) {
+        await prisma.meetingException.create({
+          data: {
+            instance: new Date(event.originalStartTime.dateTime),
+            meeting_id,
+            gcal_event_id: String(event.id),
+            start_date: new Date(event.start.dateTime),
+            title: event.summary,
+            description: event.description,
+          },
+        });
+      }
+    }
   });
 
-  for (const meeting of meetings) {
-    const event = events[meeting.gcal_event_id];
+  for (const record of records) {
+    const event = events[record.gcal_event_id];
 
     const eventStartTime = dayjs(event.start.dateTime);
-    const meetingStartTime = dayjs(meeting.start_date);
+    const meetingStartTime = dayjs(record.start_date);
 
     const update: any = {};
 
-    if (event.status.toUpperCase() !== meeting.status) {
-      console.log(`updating meeting ${meeting.id} start_date`);
+    if (event.status.toUpperCase() !== record.status) {
       update.status = event.status.toUpperCase();
     }
 
     if (!eventStartTime.isSame(meetingStartTime)) {
-      console.log(`updating meeting ${meeting.id} start_date`);
       update.start_date = eventStartTime.toDate();
-      if (event.recurrence[0]) {
+      if (event.recurrence && event.recurrence[0]) {
         update.rrule = `DTSTART;TZID=America/Los_Angeles:${eventStartTime
           .tz("America/Los_Angeles")
           .format("YYYYMMDDTHHmmss")}\n${event.recurrence[0]}`;
       }
     }
 
-    if (event.summary !== meeting.title) {
-      console.log(`updating meeting ${meeting.id} title`);
+    if (event.summary !== record.title) {
       update.title = event.summary;
     }
 
-    if (event.description !== meeting.description) {
-      console.log(`updating meeting ${meeting.id} description`);
+    if (event.description !== record.description) {
       update.description = event.description;
     }
 
     if (Object.keys(update).length > 0) {
-      await prisma.meeting.update({
-        where: { gcal_event_id: event.id },
-        data: update,
-      });
+      if (eventType === EventType.meeting) {
+        await prisma.meeting.update({
+          where: { gcal_event_id: event.id },
+          data: update,
+        });
+      } else if (eventType === EventType.meetingException) {
+        await prisma.meetingException.update({
+          where: { gcal_event_id: event.id },
+          data: update,
+        });
+      }
     }
   }
 }
-
-// syncMeetings();
-// TODO: set up watch channel
 
 async function createWatchChannel() {
   const calendar = google.calendar({ version: "v3", auth: getAuth() });
