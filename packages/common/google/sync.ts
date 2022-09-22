f12697fc69192f80b3ecd89fe68508ad54bd8610
Tim Shaker <tim@tmshkr.com>
2022-09-22 00:08:36 -0700

    progress

--- packages/common/google/sync.ts
+++ packages/common/google/sync.ts
@@ -6,19 +6,20 @@ import { getMongoClient } from "common/mongo";
 import { getEvents, getAuth } from "./index";
 import dayjs from "common/dayjs";
 const { google } = require("googleapis");
+import { patchCalendarEvent } from "common/google";
 
 export async function syncMeetings() {
   const mongoClient = await getMongoClient();
   const doc = await mongoClient
     .db()
-    .collection("config")
+    .collection("gcalSyncTokens")
     .findOne({ _id: "GCAL_SYNC_TOKEN" });
 
-  const { items, nextSyncToken } = await getEvents(
+  const { items, nextSyncToken, ...rest } = await getEvents(
     process.env.GOOGLE_CALENDAR_ID,
     doc?.syncToken
   );
-  console.log(items);
+  console.log(items, nextSyncToken, rest);
 
   const events = {};
   for (const item of items) {
@@ -29,7 +30,7 @@ export async function syncMeetings() {
 
   await mongoClient
     .db()
-    .collection("config")
+    .collection("gcalSyncTokens")
     .updateOne(
       { _id: "GCAL_SYNC_TOKEN" },
       {
@@ -62,39 +63,65 @@ async function handleUpdate(events) {
     );
     if (meeting_id) {
       // get original meeting
-      const recurring_event = await prisma.meeting.findUniqueOrThrow({
+      const recurring_event = await prisma.meeting.findUnique({
         where: {
           id: meeting_id,
         },
+        // TODO: fetch participants and add them to the new meeting
       });
+
       // create exception or new meeting
-      await prisma.meeting.create({
-        data: {
-          created_by_id: recurring_event.created_by_id,
-          end_time: new Date(event.end.dateTime),
-          gcal_event_id: String(event.id),
-          project_id: recurring_event.project_id,
-          slack_channel_id: recurring_event.slack_channel_id,
-          recurring_event_id: meeting_id,
-          rrule: event.recurrence?.[0]
-            ? `DTSTART;TZID=America/Los_Angeles:${dayjs(event.start.dateTime)
-                .tz("America/Los_Angeles")
-                .format("YYYYMMDDTHHmmss")}\n${event.recurrence[0]}`
-            : null,
-          start_time: new Date(event.start.dateTime),
-          title: String(event.summary),
-          type: "SYNCHRONOUS",
-          description: event.description,
-        },
-      });
+      if (recurring_event) {
+        const newMeeting = await prisma.meeting.create({
+          data: {
+            created_by_id: recurring_event.created_by_id,
+            end_time: new Date(event.end.dateTime),
+            gcal_event_id: event.id,
+            project_id: recurring_event.project_id,
+            slack_channel_id: recurring_event.slack_channel_id,
+            recurring_event_id: event.recurringEventId
+              ? recurring_event.id
+              : null,
+            rrule: event.recurrence?.[0]
+              ? `DTSTART;TZID=America/Los_Angeles:${dayjs(event.start.dateTime)
+                  .tz("America/Los_Angeles")
+                  .format("YYYYMMDDTHHmmss")}\n${event.recurrence[0]}`
+              : null,
+            start_time: new Date(event.start.dateTime),
+            title: event.summary,
+            type: "SYNCHRONOUS",
+            description: event.description,
+          },
+        });
+        await patchCalendarEvent(event.id, {
+          extendedProperties: {
+            private: {
+              vrms_meeting_id: newMeeting.id,
+              vrms_project_id: newMeeting.project_id,
+            },
+          },
+        });
+      }
+
+      // TODO: handle the Agenda checkin job
     }
   });
 
   for (const record of records) {
     const event = events[record.gcal_event_id];
+    if (event.status === "cancelled") {
+      await prisma.meeting.update({
+        where: { gcal_event_id: event.id },
+        data: { status: "CANCELLED" },
+      });
+      continue;
+    }
 
     const eventStartTime = dayjs(event.start.dateTime);
+    const eventEndTime = dayjs(event.end.dateTime);
+
     const meetingStartTime = dayjs(record.start_time);
+    const meetingEndTime = dayjs(record.end_time);
 
     const update: any = {};
 
@@ -104,6 +131,10 @@ async function handleUpdate(events) {
 
     if (!eventStartTime.isSame(meetingStartTime)) {
       update.start_time = eventStartTime.toDate();
+    }
+
+    if (!eventEndTime.isSame(meetingEndTime)) {
+      update.end_time = eventEndTime.toDate();
     }
 
     if (event.recurrence?.[0] !== record.rrule?.split("\n")[1]) {
