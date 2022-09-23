877473dd3b25eb90461bd1e9c2bbb84b405e888c
Tim Shaker <tim@tmshkr.com>
2022-09-22 22:30:26 -0700

    check channel id

--- apps/web/pages/api/google/calendar/watch.ts
+++ apps/web/pages/api/google/calendar/watch.ts
@@ -1,11 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { syncMeetings } from "common/google/sync";
+import { getMongoClient } from "common/mongo";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req.headers);
-  await syncMeetings();
+  const id = req.headers["x-goog-channel-id"];
+  const mongoClient = await getMongoClient();
+  const doc = await mongoClient
+    .db()
+    .collection("gcalWatchChannels")
+    .findOne({ _id: id });
+  // check that the id matches
+  if (!doc) {
+    return res.status(403).send("Forbidden");
+  }
+
   res.status(200).send("OK");
+  syncMeetings();
 }
