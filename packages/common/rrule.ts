513cbd169df2f8b2ef65fb840199e76ff3ca0e1d
Tim Shaker <tim@tmshkr.com>
2022-09-25 12:51:53 -0700

    use format string

--- packages/common/rrule.ts
+++ packages/common/rrule.ts
@@ -5,5 +5,5 @@ Returns the given date as if it was UTC, replacing any timezone offset.
 Workaround for https://github.com/jakubroztocil/rrule/issues/501
 */
 export function getPseudoUTC(date: dayjs.Dayjs): Date {
-  return new Date(date.format().slice(0, -6) + "Z");
+  return new Date(date.format("YYYY-MM-DDTHH:mm:ss[Z]"));
 }
