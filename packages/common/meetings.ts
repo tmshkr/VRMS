38f9beb1e57b5d754a68d4b6022154066a4d4298
Tim Shaker <tim@tmshkr.com>
2022-09-25 17:57:02 -0700

    scheduleNextCheckin

--- packages/common/meetings.ts
+++ packages/common/meetings.ts
@@ -1,5 +1,8 @@
 import { rrulestr } from "rrule";
 import { Meeting, MeetingException } from "@prisma/client";
+import { getAgenda } from "common/agenda";
+import prisma from "common/prisma";
+
 /*
   Returns the next occurrence of a meeting, or undefined if there are no more occurrences.
   MeetingException[] orderBy: { start_time: "asc" }
@@ -21,11 +24,17 @@ export function getNextOccurrence(
 
   let found = false;
   const instances = rule.between(start, maxDate, false, (date, i) => {
+    // if `nextInstance` is found, return false to stop the iteration
     if (found) return false;
 
+    // find the next instance that is not cancelled or tentative
     const key = date.toISOString();
-    if (!(exceptionsByInstance[key]?.status === "CANCELLED")) {
+    if (
+      !exceptionsByInstance[key] ||
+      exceptionsByInstance[key].status === "CONFIRMED"
+    ) {
       found = true;
+      return true;
     }
 
     return true;
@@ -33,6 +42,8 @@ export function getNextOccurrence(
 
   const nextInstance = instances?.pop();
 
+  // find the first exception that is after `start` and before `nextInstance`,
+  // or else find the exception that overrides `nextInstance` if it is after `nextInstance`
   const exception = meeting.exceptions.find((e) => {
     if (!e.start_time) return false;
     if (e.status !== "CONFIRMED") return false;
@@ -46,4 +57,43 @@ export function getNextOccurrence(
   });
 
   return exception?.start_time ? exception.start_time : nextInstance;
+}
+
+export async function scheduleNextCheckin(meeting_id: number, sendAt?: Date) {
+  const meeting = await prisma.meeting.findUniqueOrThrow({
+    where: { id: meeting_id },
+    include: {
+      exceptions: {
+        orderBy: { start_time: "asc" },
+      },
+    },
+  });
+
+  const nextRunAt = getNextOccurrence(meeting);
+
+  const agenda = await getAgenda();
+  const [job] = await agenda.jobs(
+    {
+      name: "sendMeetingCheckin",
+      data: { meeting_id: meeting.id },
+    },
+    undefined,
+    1
+  );
+
+  if (nextRunAt) {
+    if (job) {
+      job.schedule(nextRunAt);
+      await job.save();
+      console.log(`Updated job to run at ${nextRunAt}`);
+    } else {
+      await agenda.schedule(nextRunAt, "sendMeetingCheckin", {
+        meeting_id,
+      });
+      console.log(`Scheduled job to run at ${nextRunAt}`);
+    }
+  } else if (job) {
+    await job.remove();
+    console.log("Removed job");
+  }
 }
