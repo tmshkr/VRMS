1ab223157a1f1addbbd42199d4e80944f69f245f
Tim Shaker <tim@tmshkr.com>
2022-09-28 13:48:22 -0700

    update events

--- apps/slackbot/src/app/events.ts
+++ apps/slackbot/src/app/events.ts
@@ -1,12 +1,14 @@
 import { app } from "app";
 import { getHomeTab } from "./views/home";
 import prisma from "common/prisma";
+import cuid from "cuid";
+const slugify = require("slugify");
 
 export const registerEvents = () => {
-  app.event("app_home_opened", async ({ event, client, logger }) => {
+  app.event("app_home_opened", async ({ body, event, client, logger }) => {
     if (event.tab === "home") {
       try {
-        const home = await getHomeTab(event.user);
+        const home = await getHomeTab(event.user, body.team_id);
         const result = await client.views.publish(home);
         logger.info(result);
       } catch (error) {
@@ -14,23 +16,21 @@ export const registerEvents = () => {
       }
     }
   });
-  app.event("team_join", async ({ event, client, logger }) => {
+  app.event("team_join", async ({ event, client, logger, team_id }) => {
     const { user } = event;
     if (!user.is_bot) {
-      const row = {
-        slack_id: user.id,
-        slack_team_id: user.team_id,
-        first_name: user.profile.first_name,
-        last_name: user.profile.last_name,
-        real_name: user.real_name,
-        email: user.profile.email,
-        profile_image: user.profile.image_512,
-        timezone: user.tz,
-      };
-      await prisma.user.upsert({
-        where: { slack_id: user.id },
-        update: row,
-        create: row,
+      await prisma.user.create({
+        data: {
+          slack_id: user.id,
+          slack_team_id: user.team_id,
+          first_name: user.profile.first_name,
+          last_name: user.profile.last_name,
+          real_name: user.real_name,
+          email: user.profile.email,
+          profile_image: user.profile.image_512,
+          timezone: user.tz,
+          username: `${slugify(user.real_name.toLowerCase())}-${cuid.slug()}`,
+        },
       });
       console.log(`added user ${user.id}`);
     }
@@ -40,7 +40,12 @@ export const registerEvents = () => {
     const { user } = event;
     if (!user.is_bot) {
       await prisma.user.update({
-        where: { slack_id: user.id },
+        where: {
+          slack_id_slack_team_id: {
+            slack_id: user.id,
+            slack_team_id: user.team_id,
+          },
+        },
         data: {
           first_name: user.profile.first_name,
           last_name: user.profile.last_name,
