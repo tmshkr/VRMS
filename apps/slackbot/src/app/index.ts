1a3adf3578243cd93d8003171916ef8143df4374
Tim Shaker <tim@tmshkr.com>
2022-09-28 18:33:34 -0700

    slack install

--- apps/slackbot/src/app/index.ts
+++ apps/slackbot/src/app/index.ts
@@ -2,17 +2,98 @@
   return this.toString();
 };
 const { App } = require("@slack/bolt");
+import { getMongoClient } from "common/mongo";
 import { registerActions } from "./actions";
 import { registerEvents } from "./events";
 import { registerViewListeners } from "./views/listeners";
 import { registerCommands } from "./commands";
 
-// Initialize app
 export const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: process.env.SLACK_STATE_SECRET,
+  scopes: ["channels:history", "chat:write", "commands"],
+  port: 8000,
+  redirectUri: process.env.SLACK_INSTALL_REDIRECT_URI,
+  installerOptions: {
+    redirectUriPath: "/slack/oauth_redirect",
+  },
+  installationStore: {
+    storeInstallation: async (installation) => {
+      const mongoClient = await getMongoClient();
+      if (
+        installation.isEnterpriseInstall &&
+        installation.enterprise !== undefined
+      ) {
+        return await mongoClient
+          .db()
+          .collection("slackEnterpriseInstalls")
+          .updateOne(
+            { _id: installation.enterprise.id },
+            {
+              $set: { ...installation, updatedAt: new Date() },
+              $setOnInsert: { createdAt: new Date() },
+            },
+            { upsert: true }
+          );
+      }
+      if (installation.team !== undefined) {
+        return await mongoClient
+          .db()
+          .collection("slackTeamInstalls")
+          .updateOne(
+            { _id: installation.team.id },
+            {
+              $set: { ...installation, updatedAt: new Date() },
+              $setOnInsert: { createdAt: new Date() },
+            },
+            { upsert: true }
+          );
+      }
+      throw new Error("Failed saving installation data to installationStore");
+    },
+    fetchInstallation: async (installQuery) => {
+      const mongoClient = await getMongoClient();
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        return await mongoClient
+          .db()
+          .collection("slackEnterpriseInstalls")
+          .findOne({ _id: installQuery.enterpriseId });
+      }
+      if (installQuery.teamId !== undefined) {
+        return await mongoClient
+          .db()
+          .collection("slackTeamInstalls")
+          .findOne({ _id: installQuery.teamId });
+      }
+      throw new Error("Failed fetching installation");
+    },
+    deleteInstallation: async (installQuery) => {
+      const mongoClient = await getMongoClient();
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        return await mongoClient
+          .db()
+          .collection("slackEnterpriseInstalls")
+          .deleteOne({ _id: installQuery.enterpriseId });
+      }
+      if (installQuery.teamId !== undefined) {
+        return await mongoClient
+          .db()
+          .collection("slackTeamInstalls")
+          .deleteOne({ _id: installQuery.teamId });
+      }
+      throw new Error("Failed to delete installation");
+    },
+  },
 });
 
 registerActions();
