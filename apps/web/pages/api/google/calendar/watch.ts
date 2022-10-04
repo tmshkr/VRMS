cba1ac08770e61d84ac9838a513a1a68fdb44edd
Tim Shaker <tim@tmshkr.com>
2022-10-04 13:53:36 -0700

    log headers

--- apps/web/pages/api/google/calendar/watch.ts
+++ apps/web/pages/api/google/calendar/watch.ts
@@ -6,6 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log(req.headers);
   const id = req.headers["x-goog-channel-id"];
   const mongoClient = await getMongoClient();
   const doc = await mongoClient
