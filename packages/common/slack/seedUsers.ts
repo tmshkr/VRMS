43211037623530470681146701af67792eeca5c1
Tim Shaker <tim@tmshkr.com>
2022-10-05 23:14:09 -0700

    handle pagination

--- packages/common/slack/seedUsers.ts
+++ packages/common/slack/seedUsers.ts
@@ -10,28 +10,42 @@ export async function seedUsers(installation) {
   });
 
   console.log(`fetching users for workspace ${team.id} ${team.name}`);
-  const users = await app.client.users.list().then((res) => {
-    return res.members
-      .filter((user) => user.is_bot === false && user.name !== "slackbot")
-      .map((user) => {
-        // console.log(user);
-        return {
-          slack_id: user.id,
-          slack_team_id: user.team_id,
-          first_name: user.profile.first_name,
-          last_name: user.profile.last_name,
-          real_name: user.real_name,
-          email: user.profile.email,
-          profile_image: user.profile.image_512,
-          timezone: user.tz,
-          username: getSlug(user.real_name),
-        };
-      });
-  });
+  await createUsers(app);
+}
+
+async function createUsers(app, cursor?: string) {
+  const { users, next_cursor } = await app.client.users
+    .list({ limit: 200, cursor })
+    .then(({ members, response_metadata }) => {
+      return {
+        next_cursor: response_metadata.next_cursor,
+        users: members
+          .filter((user) => user.is_bot === false && user.name !== "slackbot")
+          .map((user) => {
+            // console.log(user);
+            return {
+              slack_id: user.id,
+              slack_team_id: user.team_id,
+              first_name: user.profile.first_name,
+              last_name: user.profile.last_name,
+              real_name: user.real_name,
+              email: user.profile.email,
+              profile_image: user.profile.image_512,
+              timezone: user.tz,
+              username: getSlug(user.real_name),
+            };
+          }),
+      };
+    });
 
   const { count } = await prisma.user.createMany({
     data: users,
     skipDuplicates: true,
   });
+
   console.log(`created ${count} users`);
+
+  if (next_cursor) {
+    await createUsers(app, next_cursor);
+  }
 }
