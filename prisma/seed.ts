7fafe32f1dee09b471108b85390a0eaffbc4bfc4
Tim Shaker <tim@tmshkr.com>
2022-10-06 21:20:56 -0700

    update seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,2 +1,5 @@
 import { seedUsers } from "common/slack/seedUsers";
-seedUsers({ bot: { token: process.env.SLACK_BOT_TOKEN }, team: "dev" });
+seedUsers({
+  bot: { token: process.env.SLACK_BOT_TOKEN },
+  team: { id: null, name: "dev" },
+});
