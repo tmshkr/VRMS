b42233d50af225c127469f68cb14779d3b681c2f
Tim Shaker <tim@tmshkr.com>
2022-10-10 15:33:40 -0700

    update seo config

---
+++ apps/web/next-seo.config.js
@@ -0,0 +1,11 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://meetbot.app",
+    site_name: "Meetbot",
+    description: "Meetbot helps you run meetings in Slack",
+  },
+  titleTemplate: "%s | Meetbot",
+  defaultTitle: "Meetbot",
+};
--- apps/web/pages/_app.jsx
+++ apps/web/pages/_app.jsx
@@ -12,36 +12,30 @@ import store from "src/store";
 import { useAppDispatch, useAppSelector } from "src/store";
 import { fetchUser, clearUser, selectUser } from "src/store/user";
 
+import SEO from "../next-seo.config";
+
 // Use of the <SessionProvider> is mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <Provider store={store}>
-        <DefaultSeo
-          openGraph={{
-            type: "website",
-            locale: "en_US",
-            url: "https://meetbot.app/",
-            site_name: "meetbot.app",
-            description: "Meetbot helps you run meetings in Slack.",
-          }}
-          titleTemplate="%s | Meetbot"
-          defaultTitle="Meetbot"
-        />
-        <Login>
-          <Dashboard>
-            {Component.auth ? (
-              <Auth pageAuth={Component.auth}>
+    <>
+      <DefaultSeo {...SEO} />
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+        <Provider store={store}>
+          <Login>
+            <Dashboard>
+              {Component.auth ? (
+                <Auth pageAuth={Component.auth}>
+                  <Component {...pageProps} />
+                </Auth>
+              ) : (
                 <Component {...pageProps} />
-              </Auth>
-            ) : (
-              <Component {...pageProps} />
-            )}
-          </Dashboard>
-        </Login>
-      </Provider>
-    </SessionProvider>
+              )}
+            </Dashboard>
+          </Login>
+        </Provider>
+      </SessionProvider>
+    </>
   );
 }
 
