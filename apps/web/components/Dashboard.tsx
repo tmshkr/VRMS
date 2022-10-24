e6fd407ec40c8696c3dc65d3799b93485476f56d
Tim Shaker <tim@tmshkr.com>
2022-10-24 11:36:22 -0700

    add optional

--- apps/web/components/Dashboard.tsx
+++ apps/web/components/Dashboard.tsx
@@ -97,8 +97,8 @@ export function Dashboard({ children }) {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {(user?.app_roles.includes("APP_ADMIN") ||
-                              user?.app_roles.includes("WORKSPACE_ADMIN")) && (
+                            {(user?.app_roles?.includes("APP_ADMIN") ||
+                              user?.app_roles?.includes("WORKSPACE_ADMIN")) && (
                               <Menu.Item>
                                 <Link
                                   href="/admin"
