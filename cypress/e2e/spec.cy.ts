30a4bbdf4915a3f09eefd6120ba47f274086b197
Tim Shaker <tim@tmshkr.com>
2022-12-05 17:03:57 -0800

    test dashboard login

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,17 @@
 describe("Sign in to Slack", () => {
-  it("signs in to Slack", () => {
-    cy.login(
-      Cypress.env("TEST_SLACK_USERNAME"),
-      Cypress.env("TEST_SLACK_PASSWORD")
+  it("signs in to the web dashboard with Slack", () => {
+    cy.visit(Cypress.env("NEXTAUTH_URL"));
+    cy.get('[data-cy="signin"]').click();
+    cy.contains("Sign in with Slack").click();
+    cy.location("search")
+      .should("contain", "redir=")
+      .then((search) => {
+        cy.login(search);
+      });
+    cy.get('[data-qa="oauth_submit_button"]').click();
+    cy.get('[data-cy="user-email"]').should(
+      "contain",
+      Cypress.env("TEST_SLACK_USERNAME")
     );
   });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,13 +2,16 @@
 
 declare namespace Cypress {
   interface Chainable<Subject = any> {
-    login(username, password): Chainable<any>;
+    login(search?: string): Chainable<any>;
   }
 }
 
-Cypress.Commands.add("login", (username, password) => {
-  cy.visit("https://meetbot-hq.slack.com/");
-  cy.get('[data-qa="login_email"]').type(username);
-  cy.get('[data-qa="login_password"]').type(password);
+Cypress.Commands.add("login", (search?: string) => {
+  const url = `https://meetbot-hq.slack.com/sign_in_with_password${
+    search || ""
+  }`;
+  cy.visit(url);
+  cy.get('[data-qa="login_email"]').type(Cypress.env("TEST_SLACK_USERNAME"));
+  cy.get('[data-qa="login_password"]').type(Cypress.env("TEST_SLACK_PASSWORD"));
   cy.get('[data-qa="signin_button"]').click();
 });
