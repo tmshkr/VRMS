30ba6e1229c3eb07b5dcb19d126559d0eae2f6a5
Tim Shaker <tim@tmshkr.com>
2022-12-06 16:23:49 -0800

    update global-setup

--- global-setup.ts
+++ global-setup.ts
@@ -1,9 +1,13 @@
 // global-setup.ts
 import { chromium, expect, FullConfig } from "@playwright/test";
+import { getMongoClient } from "common/mongo";
 
-async function globalSetup(config: FullConfig) {
+export async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
+
+  await blockHosts(page);
+
   await page.goto("https://localhost:3000");
   await page.locator('[data-test="signin-button"]').click();
   await page.getByText("Sign in with Slack").click();
@@ -21,11 +25,50 @@ async function globalSetup(config: FullConfig) {
   await page.locator('[data-qa="signin_button"]').click();
   await page.locator('[data-qa="oauth_submit_button"]').click();
   await page.locator('[data-test="add-to-slack-button"]').click();
+
   await page.locator('[data-qa="oauth_submit_button"]').click();
-  await expect(page.locator("h2")).toContainText("Thank you!");
+  await expect(page.locator("h2")).toContainText(
+    /Thank you!|Oops, Something Went Wrong!/
+  );
+
+  await expect(await getTeamInstall()).toBeTruthy();
+
   // Save signed-in state to 'storageState.json'.
   await page.context().storageState({ path: "storageState.json" });
   await browser.close();
 }
 
 export default globalSetup;
+
+async function blockHosts(page) {
+  const blockedHosts = [
+    "google-analytics.com",
+    "onetrust.com",
+    "cookielaw.org",
+    "doubleclick.net",
+  ];
+  for (const host of blockedHosts) {
+    await page.route(new RegExp(host), (route) => {
+      route.abort();
+    });
+  }
+}
+
+async function getTeamInstall() {
+  const mongoClient = await getMongoClient();
+  let tries = 0;
+  const fetch = async () =>
+    await mongoClient
+      .db()
+      .collection("slackTeamInstalls")
+      .findOne({ _id: process.env.TEST_SLACK_TEAM_ID });
+
+  while (tries < 5) {
+    const teamInstall = await fetch();
+    if (teamInstall) {
+      return teamInstall;
+    }
+    tries++;
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+}
