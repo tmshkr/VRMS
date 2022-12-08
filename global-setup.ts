c1e588ff9cc43b7a169a49ae1141a58ebda93ccb
Tim Shaker <tim@tmshkr.com>
2022-12-07 21:29:21 -0800

    update global-setup

--- global-setup.ts
+++ global-setup.ts
@@ -16,7 +16,7 @@ export async function globalSetup(config: FullConfig) {
   await page.goto(process.env.NEXTAUTH_URL);
   await page.locator('[data-test="signin-button"]').click();
   await page.getByText("Sign in with Slack").click();
-  await page.waitForURL(/.*redir=/);
+  await page.waitForURL(/redir=/);
   const search = await page.url().split("?")[1];
   await page.goto(
     `https://meetbot-hq.slack.com/sign_in_with_password?${search}`
@@ -28,14 +28,10 @@ export async function globalSetup(config: FullConfig) {
     .locator('[data-qa="login_password"]')
     .fill(process.env.TEST_SLACK_PASSWORD);
   await page.locator('[data-qa="signin_button"]').click();
-  await page
-    .locator('[data-qa="oauth_submit_button"]')
-    .click({ timeout: 60 * 1000 });
+  await page.locator('[data-qa="oauth_submit_button"]').click();
 
   await page.locator('[data-test="add-to-slack-button"]').click();
-  await page
-    .locator('[data-qa="oauth_submit_button"]')
-    .click({ timeout: 60 * 1000 });
+  await page.locator('[data-qa="oauth_submit_button"]').click();
   await expect(page.locator("h2")).toContainText(/Thank you!/);
   await expect(await getTeamInstall()).toBeTruthy();
 
